ec50e8686f4e885ae6c1a57ac25eb38f647163b1
awfixer <git@awfixer.me>
2026-03-22 11:44:37 -0700

    create awcrypt

---
+++ lib/awcrypt.ex
@@ -0,0 +1,18 @@
+defmodule Awcrypt do
+  @moduledoc """
+  Documentation for `Awcrypt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Awcrypt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/awcrypt_test.exs
@@ -0,0 +1,8 @@
+defmodule AwcryptTest do
+  use ExUnit.Case
+  doctest Awcrypt
+
+  test "greets the world" do
+    assert Awcrypt.hello() == :world
+  end
+end
